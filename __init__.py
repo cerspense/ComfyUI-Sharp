42b6e33caa759cb2ed48b213d0189b82168bbd30
PozzettiAndrea <andrea@pozzetti.it>
2026-03-11 01:17:30 +0100

    register nodes

--- __init__.py
+++ __init__.py
@@ -1,11 +1,7 @@
 import os
 import sys
+from comfy_env import register_nodes
 
-# Add this directory to sys.path so the vendored 'sharp' package can be imported
-_THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-if _THIS_DIR not in sys.path:
-    sys.path.insert(0, _THIS_DIR)
-
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = register_nodes()
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
